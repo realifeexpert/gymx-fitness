c9b24b3dbae9fd2d1ea3b7f2e7d58de8b4128c31
jadhavharshh <harshjadhavconnect@gmail.com>
2025-06-11 16:51:04 +0530

    Add exercise fetching functionality to Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Box } from "@mui/material";
 import HeroBanner from "../components/HeroBanner";
 import SearchExercises from "../components/SearchExercises";
 import Exercises from "../components/Exercises";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 const Home = () => {
   const [exercises, setExercises] = useState([]);
   const [bodyPart, setBodyPart] = useState("all");
+
+  useEffect(() => {
+    const fetchExercisesByBodyPart = async () => {
+      let exercisesData = [];
+      
+      if (bodyPart === 'all') {
+        exercisesData = await fetchData(
+          'https://exercisedb.p.rapidapi.com/exercises', 
+          exerciseOptions
+        );
+      } else {
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, 
+          exerciseOptions
+        );
+      }
+      
+      setExercises(exercisesData);
+    };
+
+    fetchExercisesByBodyPart();
+  }, [bodyPart]);
 
   return (
     <Box>
@@ -18,7 +41,7 @@ const Home = () => {
       />
 
       <Exercises
-        exercises={exercises} // ✅ THIS WAS MISSING
+        exercises={exercises}
         setExercises={setExercises}
         bodyPart={bodyPart}
       />
